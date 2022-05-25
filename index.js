948a19952c943937b304f16e4fcafd6098916229
Md Shahriur Ahmed <tasnovakanta2000@gmail.com>
2022-05-26 04:50:15 +0600

    jwt token verified

--- index.js
+++ index.js
@@ -15,7 +15,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
 
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 async function run() {
@@ -36,7 +50,7 @@ async function run() {
         });
 
         // view tools by id
-        app.get('/purchase/:id', async (req, res) => {
+        app.get('/purchase/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const tool = await toolsCollection.findOne(query);
@@ -55,9 +69,9 @@ async function run() {
 
         //view orders of an user
 
-        app.get('/myorders', async (req, res) => {
+        app.get('/myorders', verifyJWT, async (req, res) => {
             const userEmail = req.query.userEmail;
-            console.log(userEmail);
+
             const query = { clientEmail: userEmail };
             const orders = await orderCollection.find(query).toArray();
             res.send(orders);
@@ -73,8 +87,11 @@ async function run() {
             const updateDoc = {
                 $set: user,
             };
+
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
             const result = await userCollection.updateOne(filter, updateDoc, options)
-            res.send(result);
+            res.send({ result, token });
         })
 
 
