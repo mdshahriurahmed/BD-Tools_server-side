737b414ca04f11fcb895bffe0490228b2fd4d261
mdshahriurahmed <shahriurahmedcse@gmail.com>
2022-12-25 12:29:57 +0600

    dsfs

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function verifyJWT(req, res, next) {
 async function run() {
     try {
         
-         await client.connect();
+         client.connect();
 
         const toolsCollection = client.db('bd-tools').collection('tools');
         const orderCollection = client.db('bd-tools').collection('orders');
