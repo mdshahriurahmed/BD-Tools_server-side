8a818d0740e37c876d75b33ad11bb86b5d2d22cc
mdshahriurahmed <shahriurahmedcse@gmail.com>
2022-12-25 12:21:17 +0600

    projectr edited

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ function verifyJWT(req, res, next) {
 
 async function run() {
     try {
-        // await client.connect();
+
+          client.connect();
+
         const toolsCollection = client.db('bd-tools').collection('tools');
         const orderCollection = client.db('bd-tools').collection('orders');
         const userCollection = client.db('bd-tools').collection('users');
